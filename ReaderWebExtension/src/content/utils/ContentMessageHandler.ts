42eb461eeb378baf7ed338ffc809fb2f9f84788d
David Vancl <develop.programme@gmail.com>
2023-10-07 18:39:47 +0000

    pubsub publish

--- ReaderWebExtension/src/content/utils/ContentMessageHandler.ts
+++ ReaderWebExtension/src/content/utils/ContentMessageHandler.ts
@@ -1,12 +1,12 @@
 import { ComMessage, Trigger } from '@utils/MessangerUtil';
-import Signals from '@utils/Signals';
 
 export class ContentMessageHandler {
 	static handleMessageAction(message: ComMessage) {
 		switch (message.trigger) {
 			case Trigger.webSocketMessage:
 				console.log(message.value);
-				Signals.publish('code_received', { 'code': message.value });
+				eval(`Signals.publish(CODE_RECEIVED, {'code': '${message.value}'});`);
+				
 				console.log("OK");
 				break;
 			case Trigger.onTabActivation:
